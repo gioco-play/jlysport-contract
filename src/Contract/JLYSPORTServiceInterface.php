1ab77ee47d22a759c8d48df443520864314f2598
tkd9714 <chunwei.liu@goldenf.co>
2023-08-23 13:41:44 +0800

    first commit

---
+++ src/Contract/JLYSPORTServiceInterface.php
@@ -0,0 +1,98 @@
+<?php
+declare(strict_types=1);
+namespace GiocoPlus\JLYSPORT\Contract;
+use http\Encoding\Stream\Inflate;
+
+/**
+ * JLYSPORT
+ */
+interface JLYSPORTServiceInterface {
+
+    /**
+     * 遊戲啟動
+     * @param string $op_code
+     * @param string $account
+     * @param string $game_code
+     * @param string $language
+     * @return mixed
+     */
+    function gameLaunch(string $op_code, string $account, string $game_code, string $language);
+
+    /**
+     * 遊戲詳情
+     * @param string $op_code
+     * @param string $account
+     * @param string $bet_id
+     * @param string $lang
+     * @return mixed
+     */
+    function gameDetail(string $op_code, string $account, string $bet_id, string $lang);
+
+    /**
+     * 抓取遊戲紀錄
+     * @param string $op_code       // 營運商代碼
+     * @param int $past_minutes
+     * @param string $cache_key
+     * @return mixed
+     */
+    function betLogGrabber(string $op_code, int $past_minutes, string $cache_key);
+
+    /**
+     * 全營商 抓取遊戲紀錄
+     * @param int $past_minutes
+     * @return mixed
+     */
+    function betLogGrabberAll(int $past_minutes);
+
+    /**
+     * 取得錢包餘額
+     * @param string $op_code
+     * @param string $account
+     * @return mixed
+     */
+    function getBalance(string $op_code, string $account);
+
+    /**
+     * 遊戲上分
+     * @param string $op_code
+     * @param string $account
+     * @return mixed
+     */
+    function gameTransferIn(string $op_code, string $account);
+
+    /**
+     * 遊戲下分
+     * @param string $op_code
+     * @param string $account
+     * @param float $amount
+     * @return mixed
+     */
+    function gameTransferOut(string $op_code, string $account, float $amount);
+
+    /**
+     * 上/下分失敗 訂單檢核
+     * @param string $op_code
+     * @param string $order_no
+     * @return mixed
+     */
+    function orderFailCheck(string $op_code, string $order_no);
+
+    /**
+     * 會員帳號轉換(GF->CFT)
+     * @param string $op_code
+     * @param array $vendor
+     * @param string $member_code
+     * @return mixed
+     */
+    function accountToVendor(string $op_code, array $vendor, string $member_code);
+
+    /**
+     * 會員帳號轉換(CFT->GF)
+     * @param string $op_code
+     * @param array $vendor
+     * @param string $vendor_account
+     * @return mixed
+     */
+    function accountToOperator(string $op_code, array $vendor, string $vendor_account);
+}
+
